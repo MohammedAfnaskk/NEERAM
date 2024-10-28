ddc34683863e5083295251e6ae97d7b4dc0b8c7d
Mohammed Afnas <afnaskkvlr@gmail.com>
2024-10-28 13:13:48 +0530

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3002
+  server: {
+    port: 3002
+  },
+  base: '/', // Base URL for the app, useful if deploying under a subpath
+  build: {
+    outDir: 'dist' // Ensure this matches 'distDir' in your vercel.json
   }
-})
+});
