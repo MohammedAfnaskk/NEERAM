36dc23b75cffe4061100745e0a73526c052b64ff
Mohammed Afnas <afnaskkvlr@gmail.com>
2024-10-28 13:00:33 +0530

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import HomePage from './components/HomePage/content'
-import About from './components/AboutPage/content';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Oilmill from './components/OilmillPage/oilmillpage';
-import Contact from './components/ContactPage/contact';
-import WhatsApp from './components/ContactPage/whatsapp';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage/content";
+import About from "./components/AboutPage/content";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Oilmill from "./components/OilmillPage/oilmillpage";
+import Contact from "./components/ContactPage/contact";
+import WhatsApp from "./components/ContactPage/whatsapp";
 
 function App() {
   return (
-    <Router> 
-    <div>
-     <Navbar/>  
-    </div>
-    <Routes>
-      <Route path='/' element={<HomePage/>}/> 
-      <Route path='/about' element={<About/>}/>
-      <Route path='/oilmill' element={<Oilmill/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-
-    </Routes>
-    <WhatsApp/>
-    <Footer/>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/oilmill" element={<Oilmill />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <WhatsApp />
+      <Footer />
     </Router>
   );
 }
 
-export default App
+export default App;
